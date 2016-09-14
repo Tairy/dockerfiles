5bf033cf3de97025d31cab68dd391f1233175b1b
Tairy <tairyguo@gmail.com>
2016-09-14 11:21:27 +0800

    fix!

---
+++ simple-nginx-php/config/local_loader.php
@@ -0,0 +1,18 @@
+<?php
+define('__DEFINED__', true);
+define('__DEBUG__', true);
+define('__PRODUCT__', true);
+define('__REDIS_SERVER__', '192.168.1.7:6379');
+define('__REDIS_TMP_SERVER__', '192.168.1.15:6379');
+define('__REDIS_CLUSTER_SERVER__', '192.168.1.26:6379');
+define('__SEARCH_SERVER__', '192.168.1.6:8383');
+define('__INDEX_SERVER__', '192.168.1.6:8384');
+define('__DB_SERVER__', '192.168.1.5:segmentfault');
+define('__DB_SLAVE_SERVERS__', '192.168.1.24:segmentfault');
+define('__STATIC_URL__', '/build');
+define('__MONGO_SERVER__', 'mongodb://192.168.1.17');
+define('__STATIC_VERSION__', NULL);
+define('__RES_DIR__', '/www/segmentfault/res');
+define('__BETA__', true);
+define('__MEMCACHED_SERVER__', '192.168.1.14:11211,192.168.1.25:11211');
+define('__ASYNC_URL__', '192.168.1.10:2345');
